ea6513d52febe0ba8ffa0fffe5089465949deb56
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2018-08-17 13:00:09 -0300

    Navegacao

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 
-//decorator para indicar que esta classe e uma pagina
+//decorator para indicar que esta classe e uma pagina, e assim ser referenciado como string no app.components.ts
 @IonicPage()
 @Component({
   selector: 'page-home',
@@ -11,6 +11,10 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+
+  login(){
+    this.navCtrl.setRoot("CategoriasPage");
   }
 
 }
