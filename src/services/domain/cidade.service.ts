a21bdec552efd3de4356134ab13fb7591b18ce6b
Fabricio Magalhaes <fabricio.lvm@gmail.com>
2018-08-19 22:42:14 -0300

    Povoando dinamincamente cidade e estado

---
+++ src/services/domain/cidade.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { API_CONFIG } from "../../config/api.config";
+import { CidadeDTO } from "../../models/cidade.dto";
+import { Observable } from "rxjs/Rx";
+
+@Injectable()
+export class CidadeService{
+
+    constructor(public http : HttpClient) {
+
+    }
+
+    //metodo para retornar uma lista de cidades de um dado estado
+    findAll(estado_id : string) : Observable<CidadeDTO[]> {
+        return this.http.get<CidadeDTO[]>(`${API_CONFIG.baseUrl}/estados/${estado_id}/cidades`);
+}
+}
